0dbabfe4a18470e37ac81508606a375e182e5d78
edilioGF <EGarcia@contractor.nexstardigital.com>
2020-08-12 00:40:25 -0400

    function to calculate calories per day

--- dieta.py
+++ dieta.py
@@ -2,33 +2,37 @@ import schemeParser
 import math
 import views
 
-def calcularGrasaCorporal(sexo, peso, edad, altura, window ):
+def calcularGrasaCorporal(sexo, peso, edad, altura, window, tmb ):
 
-        IMC = (int)(float(peso))/((int)(int(altura)/100)^2)
-        mult1 = "(* 1.20 " + str(IMC) + ")"
-        mult2 = "(* 0.23 " + str(edad) + ")"
-        if (sexo == "Masculino"):
-            mult3 = "(* 10.8 1)"
-        else:
-            mult3 = "(* 10.8 0)"
+    IMC = (int)(float(peso))/((int)(int(altura)/100)^2)
+    mult1 = "(* 1.20 " + str(IMC) + ")"
+    mult2 = "(* 0.23 " + str(edad) + ")"
+    if (sexo == "Masculino"):
+        mult3 = "(* 10.8 1)"
+    else:
+        mult3 = "(* 10.8 0)"
 
-        sum = "(+ " + mult1 + " " + mult2 + ")"
-        actualSum = schemeParser.evaluate(sum)
-        grasaCorporal = "(- (- " + str(actualSum) + " " + mult3 +") 5.4)"
-        value =schemeParser.evaluate(grasaCorporal)
-        views.printToScreen(value, 'Grasa Corporal', 10, window)
+    sum = "(+ " + mult1 + " " + mult2 + ")"
+    actualSum = schemeParser.evaluate(sum)
+    grasaCorporal = "(- (- " + str(actualSum) + " " + mult3 +") 5.4)"
+    value = schemeParser.evaluate(grasaCorporal)
+    calories = calcularCaloriasDiarias(sexo, peso, altura, edad, tmb) 
+    views.printToScreen(value, 'Grasa Corporal', 10, window)
 
 
 def calcularCaloriasDiarias(sexo,peso, altura, edad, tmb):
-
-    Hombres: TMB = 66 + (13,7 x peso en kg) + (5 x altura en cm) - (6,75 x edad en años)
-    Mujeres: TMB = 655 + (9,6 x peso en kg) + (1.8 x altura en cm) - (4,7 x edad en años)
 
     if (sexo == "Masculino"):
         mult1 = "(* 13.7 " + str(peso) + ")"
         mult2 = "(* 5 " + str(altura) + ")"
         mult3 = "(* 6.75 " + str(edad) + ")"
+        suma = "(+ 66 " + mult1 + " " + mult2 + " " + mult3 + ")" 
     else:
         mult1 = "(* 9.6 " + str(peso) + ")"
         mult2 = "(* 1.8 " + str(altura) + ")"
-        mult3 = "(* 6.75 " + str(edad) + ")"
+        mult3 = "(* 4.7 " + str(edad) + ")"
+        suma = "(+ 665 " + mult1 + " " + mult2 + " " + mult3 + ")"
+
+    result = "(* " + tmb + " " + suma + ")"
+    calories = schemeParser.evaluate(result)
+    return calories
